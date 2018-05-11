a8d206b2abec14da40ec06c519c891997b56a135
CrabAss <jan.hanyu@outlook.com>
2018-05-11 15:27:33 +0800

    transaction init

---
+++ db/transaction.js
@@ -0,0 +1,40 @@
+let mongoose = require("mongoose");
+
+let transactionSchema = new mongoose.Schema({
+  sellerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  buyerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  recordID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  addressID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  readStatus: {
+    type: Boolean,
+    required: true,
+    default: false  // unread
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: new Date()
+  },
+  transactionStatus: {
+    type: String,
+    required: true,
+    enum: ["Created", "Accepted", "Rejected", "Sent", "Complete"],
+    default: "Created"
+  }
+});
+
+
+let Transaction = mongoose.model('Transaction', transactionSchema);
+module.exports = Transaction;
--- routes/transaction.js
+++ routes/transaction.js
@@ -1,9 +1,22 @@
+/*
+交易系统
+    交易详情页 todo
+    买家 todo
+        正在进行的交易【包括是否接受等等】 todo
+        已完成的交易 todo
+    卖家 todo
+        正在进行的交易【也可以拒绝】 todo
+        已完成的交易 todo
+查询交易 todo
+*/
+
 let express = require('express');
+let transactionDB = require('../db/transaction');
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
